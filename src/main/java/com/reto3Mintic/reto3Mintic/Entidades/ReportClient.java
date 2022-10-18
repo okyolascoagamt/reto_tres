d1e98940a1a1ad255b81558ed66061cf63a5749c
OKY EDGARDO OLASCOAGA TORREGLOSA <oky.olascoaga.mt@usa.edu.co>
2022-10-17 19:35:25 -0500

    version 12 reto 4

---
+++ src/main/java/com/reto3Mintic/reto3Mintic/Entidades/ReportClient.java
@@ -0,0 +1,9 @@
+package com.reto3Mintic.reto3Mintic.Entidades;
+
+
+import java.io.Serializable;
+
+public class ReportClient implements Serializable {
+    public int total;
+    public Client client;
+}
---
+++ src/main/java/com/reto3Mintic/reto3Mintic/Entidades/Status.java
@@ -0,0 +1,11 @@
+package com.reto3Mintic.reto3Mintic.Entidades;
+
+import java.io.Serializable;
+
+
+
+public class Status implements Serializable {
+    public int completed;
+    public int cancelled;
+
+}
